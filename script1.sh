a9aa4d67781d1be050ff0e16c5f3da7b87411d4e
Sarah Quinn <sarahq@kainos.com>
2017-09-21 14:45:18 +0100

    updated to include error handling to avoid overwritting files

--- script1.sh
+++ script1.sh
@@ -1,9 +1,32 @@
+#!/bin/bash
+
+
+if [ !  -d "team" ]
+then
 mkdir team
 cd team
+
+if [ ! -d "folders" ]
+then 
 mkdir folders
 cd folders
+
+if [ ! -d "folder"$1 ]
+then
 mkdir folder$1
 cd folder$1
 echo "Hello I am a file, my name is file$1"> file$1.txt
 cat file$1.txt
 
+
+else 
+echo "Directory already exists - operation aborted"
+fi
+
+else
+echo "Directory already exists - operation aborted"
+fi 
+
+else
+echo "Directory already exists - operation aborted"
+fi 
