524c89b6c30b44ede391bc9d06b3ec3282129905
Sarah Quinn <sarahq@kainos.com>
2017-09-21 15:14:58 +0100

    fixed to accomodate for parameter checking and additional error handling

--- script1.sh
+++ script1.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
 
+if [ ! $# ==  2 ]
+then 
+echo $#"/2 arguements provided, please run again"
+exit
+fi
+
+
 if [ !  -d "team" ]
 then
 mkdir team
+fi
+
 cd team
 
 if [ ! -d "folders" ]
 then 
 mkdir folders
+fi
+
 cd folders
 
 if [ ! -d "folder"$1 ]
 then
 mkdir folder$1
 cd folder$1
-echo "Hello I am a file, my name is file$1"> file$1.txt
-cat file$1.txt
-
+echo "Hello I am a file, my name is file$2"> file$2.txt
+cat file$2.txt
 
 else 
 echo "Directory already exists - operation aborted"
+
 fi
 
-else
-echo "Directory already exists - operation aborted"
-fi 
-
-else
-echo "Directory already exists - operation aborted"
-fi 
+echo "operation complete"
